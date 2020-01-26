7a2bf3734f97ca437da2d767ff5c4d58d67e8d2c
Pete Wilcoxen <wilcoxen@syr.edu>
2020-01-26 17:36:43 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,18 +17,18 @@ for n in a_list:
 print("\nInitial list:",a_list)
 print("Cubes via for loop:",a_cubes)
 
-#  Building the cubes via a list comprehension. This computes
-#  all the cubes at oncethe same thing in a more compact (and i
-#  often clearer) way.
+#  Build the cubes via a list comprehension. This computes
+#  all the cubes at once. It's more compact and often
+#  clearer than using a regular for loop.
 
 cubes = [n**3 for n in a_list]
 
 print("\nCubes via list comprehension",cubes)
 
-#  Using range to build lists. A call like range(a,b) call produces 
+#  Use range to build the cubes. A call like range(a,b) produces 
 #  a sequence that starts with 'a' and ends just before 'b'. In the 
 #  example below, b_list will start at 1 and end at 5, just like
-#  a_list.
+#  a_list. As a result, the cubes will be identical.
 
 b_list = range(1,6)
 b_cubes = [b**3 for b in b_list]
